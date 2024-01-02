ee3a6b41e174288cbe6dc23e909df5c00359b33a
amit-negi-23 <amitnegiit23@gmail.com>
2024-01-02 16:47:19 +0530

    third commit

--- firstreact/src/components/Contact.js
+++ firstreact/src/components/Contact.js
@@ -2,9 +2,6 @@ import React from 'react'
 
 export default function Contact(){
     return(
-       <div>
-         <h1>Contact us page</h1>
-        <p>Hello </p>
-       </div>
+        <h1>Contact us page</h1>
     )
 }
