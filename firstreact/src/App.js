7d0a55ac51138de8a850727f06926450d65a921c
amit-negi-23 <amitnegiit23@gmail.com>
2024-01-04 21:37:22 +0530

    dropdown

--- firstreact/src/App.js
+++ firstreact/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './App.css'
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import Home from "./components/Home";
 import About from "./components/About.js";
@@ -18,9 +19,24 @@ function App() {
                     <li className="nav-item"><Link to="/services" className='nav-link'>Services</Link></li>
                     <li className="nav-item"><Link to="/contact" className='nav-link'>Contact</Link></li>
 
+                    <div className="nav-item dropdown-x">
+                      <Link className="nav-link">Dropdown</Link>
+                              <div className="dropdown-menu-x">
+                                    <div><Link to="/home" className='nav-link'>Home</Link></div>
+                                    <div><Link to="/about" className='nav-link'>About</Link></div>
+                                    <div><Link to="/contact" className='nav-link'>Contact</Link></div>
+                                    <div><Link to="/services" className='nav-link'>Services</Link></div>
+                                    
+                                    
+                              </div>
+                    </div>
                     
               </ul>
            </nav>
+
+            
+
+
                     <Routes>
                           <Route path="/" element={<Home/>}/>
                           <Route path="/about" element={<About/>}/>
